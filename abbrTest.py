8e4e157dd993d43fdf442eb41ac0ae557013f770
Breeze-G13 <103465271+Breeze-G13@users.noreply.github.com>
2022-04-12 06:46:07 +0800

    Add files via upload

---
+++ abbrTest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Author: Breeze.G (GAO Wentao) from Capgemini China JTP 0411/2022 cohort
+
+# import some libraries
+import pandas as pd
+import numpy as np
+import random
+
+# read the abbreviation table
+data = pd.read_csv('abbr.csv')
+df = data.copy(deep=True)
+
+
+# In[2]:
+
+
+df.loc[:, "testCounts"] = 0
+total = len(df)
+toTest = df.copy(deep=True)
+tested = pd.DataFrame()
+
+while len(toTest)>0: 
+    i = random.randint(0, len(toTest))
+    df.loc[i, "testCounts"] += 1
+    prompt = "[" + toTest.loc[i, "label"].upper() + "] " + toTest.loc[i, "abbr"]
+    key = toTest.loc[i, "full"]
+    ans = input(prompt+" = ")
+    if ans in ["stop", "Stop", "STOP", "done", "Done", "DONE"]:
+        print(">> Test halted. \n")
+        df.loc[i, "testCounts"] -= 1
+        break
+    elif ans.upper()==key.upper():
+        print(">> Correct! \n")
+        tested = tested.append(toTest.loc[i])
+        if df.loc[i, "testCounts"] == 1:
+            tested.loc[i, "correct"] = True
+        toTest = toTest.drop(i).reset_index(drop=True)
+    else:
+        print(">> Incorrect.", prompt, "=", key, "\n")
+        tested = tested.append(toTest.loc[i])
+
+tested.drop_duplicates(subset="abbr", keep=False, inplace=True)
+oneTimeCorrect = len(tested.loc[tested.correct==True])
+print("Among", len(tested), "unique abbreviations tested,", oneTimeCorrect, "was correct at first time.")
+print("One-time correct rate is %.2f%%" % round(oneTimeCorrect/len(tested)*100, 2))
+
